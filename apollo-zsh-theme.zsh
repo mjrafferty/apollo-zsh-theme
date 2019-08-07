80b099d7320ca609e048bd5f4808b8c00c445dac
Matt Rafferty <mjrafferty0@gmail.com>
2019-08-07 01:18:32 -0400

    Disabled compilation of functions and modules for now

--- apollo-zsh-theme.zsh
+++ apollo-zsh-theme.zsh
@@ -3,13 +3,13 @@
 typeset -g __APOLLO_INSTALL_DIR="${(%):-%N}"
 __APOLLO_INSTALL_DIR="${__APOLLO_INSTALL_DIR%/*}"
 
-[[ ! -e "${__APOLLO_INSTALL_DIR}/functions.zwc" ]] \
-  && zcompile "${__APOLLO_INSTALL_DIR}/functions.zwc" "${__APOLLO_INSTALL_DIR}/functions/"*
+#[[ ! -e "${__APOLLO_INSTALL_DIR}/functions.zwc" ]] \
+  #&& zcompile "${__APOLLO_INSTALL_DIR}/functions.zwc" "${__APOLLO_INSTALL_DIR}/functions/"*
 
-[[ ! -e "${__APOLLO_INSTALL_DIR}/modules.zwc" ]] \
-  && zcompile "${__APOLLO_INSTALL_DIR}/modules.zwc" "${__APOLLO_INSTALL_DIR}/modules/"*
+#[[ ! -e "${__APOLLO_INSTALL_DIR}/modules.zwc" ]] \
+  #&& zcompile "${__APOLLO_INSTALL_DIR}/modules.zwc" "${__APOLLO_INSTALL_DIR}/modules/"*
 
-fpath+=("${__APOLLO_INSTALL_DIR}/functions.zwc")
+fpath+=("${__APOLLO_INSTALL_DIR}/functions")
 
 autoload -Uz prompt_apollo_setup
 prompt_apollo_setup
