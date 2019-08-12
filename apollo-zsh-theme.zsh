d1f4ecf9d159493b3c5e7eb36676440e6cb7bd15
Matt Rafferty <mjrafferty0@gmail.com>
2019-08-11 22:01:28 -0400

    Reverse blend option for separators.

--- apollo-zsh-theme.zsh
+++ apollo-zsh-theme.zsh
@@ -3,7 +3,7 @@
 typeset -g __APOLLO_INSTALL_DIR="${(%):-%N}"
 __APOLLO_INSTALL_DIR="${__APOLLO_INSTALL_DIR%/*}"
 
-__apollo_compile() {
+{
 
   for dir in functions modules conf; do
     [[ ! -s "${__APOLLO_INSTALL_DIR}/${dir}.zwc" \
@@ -11,13 +11,10 @@ __apollo_compile() {
       && zcompile "${__APOLLO_INSTALL_DIR}/${dir}.zwc" "${__APOLLO_INSTALL_DIR}/${dir}/"*
   done
 
-}
-
-__apollo_compile &!
-unfunction __apollo_compile
+} &!
 
 if [[ ${fpath[(ie)"${__APOLLO_INSTALL_DIR}/functions"]} -gt ${#fpath} ]]; then
-    fpath+=("${__APOLLO_INSTALL_DIR}/functions.zwc" "${__APOLLO_INSTALL_DIR}/functions")
+  fpath+=("${__APOLLO_INSTALL_DIR}/functions.zwc" "${__APOLLO_INSTALL_DIR}/functions")
 fi
 
 autoload -Uz prompt_apollo_setup
